9755712528c233f30f64eaf0f4a8e5390f84cf09
SeongBaeJeon <tjdqo6042@naver.com>
2017-04-05 18:00:50 +0900

    modify isEmpty()

--- src/main/java/univ/lecture/Transform.java
+++ src/main/java/univ/lecture/Transform.java
@@ -12,11 +12,11 @@ public class Transform {
 		Stack stack = new Stack<String>();
 		String[] post = new String[args.length];
 		for (int i = 0; i < args.length; i++) {
-			if (stack.isEmpty()) {
-				stack.push(args[i]);
-			} else if (precedence(args[i]) == 0 || precedence(args[i]) == 2 || precedence(args[i]) == 4
+			if (precedence(args[i]) == 0 || precedence(args[i]) == 2 || precedence(args[i]) == 4
 					|| precedence(args[i]) == 9) {/* ()ASMD가 나왔을 때 */
-				if (precedence(args[i]) == 2) {/* A+ S- 일때 */
+				if (stack.isEmpty()) {
+					stack.push(args[i]);
+				} else if (precedence(args[i]) == 2) {/* A+ S- 일때 */
 					while (!stack.isEmpty() && precedence(args[i]) <= precedence((String) stack.peek())) {
 						if (precedence((String) stack.peek()) == 2 || precedence((String) stack.peek()) == 4) {
 							post[index++] = (String) stack.pop();
