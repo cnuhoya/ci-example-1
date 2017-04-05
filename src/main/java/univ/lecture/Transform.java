9b40544783a9635295f374af6d1d2b3d970bcb22
SeongBaeJeon <tjdqo6042@naver.com>
2017-04-05 17:34:54 +0900

    First Commit

---
+++ src/main/java/univ/lecture/Transform.java
@@ -0,0 +1,27 @@
+package univ.lecture;
+
+import java.util.Stack;
+
+public class Transform {
+	private int index = 0;
+
+	/* 구현에 필요한 변수는 임의로 선언할 것 */
+	public String[] infixToPostfix(String[] args) {
+		// 중위식을 후위식으로 변환한 결과를 String 배열로 반환하는 코드를 작성하고,
+		// 마지막에 변환된 후위식을 출력함.
+		Stack stack = new Stack<String>();
+		String[] post = new String[args.length];
+		for (int i = 0; i < args.length; i++) {
+			if (precedence(args[i]) == 0 || precedence(args[i]) == 2 || precedence(args[i]) == 4
+					|| precedence(args[i]) == 9) {/* ()ASMD가 나왔을 때 */
+			} else
+				/* 문자가 아닌 숫자일 경우 post배열에 저장 */
+				post[index++] = args[i];
+		}
+		return args;
+	}
+
+	public int precedence(String token) {
+		return 0;
+	}
+}
