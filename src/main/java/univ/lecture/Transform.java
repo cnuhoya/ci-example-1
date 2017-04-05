9d671b902e7fbbb1078b5b0d4c25b46dc7f1f78a
SeongBaeJeon <tjdqo6042@naver.com>
2017-04-05 17:58:28 +0900

    add ASMD

--- src/main/java/univ/lecture/Transform.java
+++ src/main/java/univ/lecture/Transform.java
@@ -14,11 +14,30 @@ public class Transform {
 		for (int i = 0; i < args.length; i++) {
 			if (precedence(args[i]) == 0 || precedence(args[i]) == 2 || precedence(args[i]) == 4
 					|| precedence(args[i]) == 9) {/* ()ASMD가 나왔을 때 */
-			} else
-				/* 문자가 아닌 숫자일 경우 post배열에 저장 */
-				post[index++] = args[i];
+				if (precedence(args[i]) == 2) {/* A+ S- 일때 */
+					while (!stack.isEmpty() && precedence(args[i]) <= precedence((String) stack.peek())) {
+						if (precedence((String) stack.peek()) == 2 || precedence((String) stack.peek()) == 4) {
+							post[index++] = (String) stack.pop();
+						} else if (precedence((String) stack.peek()) == 0) {
+							stack.push(args[i]);
+						}
+					}
+					stack.push(args[i]);
+				} else if (precedence(args[i]) == 4) {/* D/ M* 일때 */
+					while (!stack.isEmpty() && precedence(args[i]) <= precedence((String) stack.peek())) {
+						if (precedence((String) stack.peek()) == 2) {
+							stack.push(args[i]);
+						} else if (precedence((String) stack.peek()) == 4) {
+							post[index++] = (String) stack.pop();
+							stack.push(args[i]);
+						}
+					}
+				} else
+					/* 문자가 아닌 숫자일 경우 post배열에 저장 */
+					post[index++] = args[i];
+			}
 		}
-		return args;
+		return post;
 	}
 
 	public int precedence(String token) {
