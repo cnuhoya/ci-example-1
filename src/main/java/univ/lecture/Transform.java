9f622db4c2dbcec48c2cb03c2767c2ce59f4c546
LeeChungWan <cnddhks846@naver.com>
2017-04-05 19:08:29 +0900

    (, ) 우선순위 부여

--- src/main/java/univ/lecture/Transform.java
+++ src/main/java/univ/lecture/Transform.java
@@ -22,10 +22,14 @@ public class Transform {
 	}
 
 	public int precedence(String token) {
-		if (token.equals("A") || token.equals("S")) {
+		if (token.equals("(")) {
+			return 0;
+		} else if (token.equals("A") || token.equals("S")) {
 			return 2;
 		} else if (token.equals("M") || token.equals("D")) {
 			return 4;
+		} else if (token.equals(")")) {
+			return 9;
 		}
 		return -1;
 	}
