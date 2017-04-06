66ff19ccb71d56f5fffc442862babbe4a7e99bc7
kimjinhyeok <persona5173@gmail.com>
2017-04-06 22:43:01 +0900

    modified error

--- src/test/java/univ/lecture/CalculatorTest.java
+++ src/test/java/univ/lecture/CalculatorTest.java
@@ -34,6 +34,8 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         int output = calc.calculate("4*5");
         assertThat(output, is(20));
+        output = calc.calculate("5*6*7");
+        assertThat(output, is(20));
     }
     
     @Test
